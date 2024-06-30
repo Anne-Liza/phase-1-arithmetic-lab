c45b4c4d0f6366763c6ec14f8fdb889faa457561
Anne <annewangui210@gmail.com>
2024-06-30 19:36:59 +0300

    Adds arithmetic lab phase-1

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+//multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+//random integer greater than 0
+let random = Math.floor(Math.random() * 100 + 1);
+console.log(random);
+
+//remainder is equal to 4
+const num3 = 29;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+//will return 20 as the highest number
+const max =Math.max(1,5,10,20);
